16379ad07f120dabd0c771c8822067df97e65e36
jbofill10 <juan.bofill10@gmail.com>
2020-03-25 01:48:42 -0400

    Add incub hist

--- EDA/task1_transmission_incubation/Incubation.py
+++ EDA/task1_transmission_incubation/Incubation.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.style as style
 
+
 def incubation(df):
     style.use('seaborn-poster')
     style.use('ggplot')
@@ -10,23 +11,34 @@ def incubation(df):
     incubation_occurrences = list()
 
     body_text = df['body'].values
-    
+    counter = 0
     for text in body_text:
         for sent in text.split('. '):
             if 'incubation' in sent:
-                temp = re.findall('\d[1-9]{1,2}\sday', sent)
+                temp = re.findall('[1-9]{1,2}\S*\sday|s$', sent)
                 if len(temp) > 0:
                     for find in temp:
-                        incubation_occurrences.append(find[0:find.index(' ')])
+                        find = re.sub('\D', ' ', find)
+                        house = find.strip().split(' ')
+                        for nums in house:
+                            try:
+                                incubation_occurrences.append(int(nums))
+                            except:
+                                continue
 
-    incubation_df = pd.DataFrame({'days':incubation_occurrences}, index=range(0,len(incubation_occurrences)))
-    incubation_df=incubation_df.sort_index()
-    
-    print(incubation_df['days'].value_counts().sort_index())
-    
-    
-    plt.hist(incubation_df['days'].value_counts(), bins=50)
-    plt.xlabel("Incubation Time in Days")
-    plt.xlim(0,96)
-    plt.ylabel("")
+    print(len(incubation_occurrences))
+    print(counter)
+
+    incubation_df = pd.DataFrame({'days': incubation_occurrences}, index=range(0, len(incubation_occurrences)))
+    incubation_df = incubation_df.sort_index()
+
+    print(int(round(incubation_df['days'].mean())))
+
+    plt.hist(incubation_df['days'].value_counts(), bins=75, color='purple')
+    plt.xlabel("Incubation Time in Days", fontsize=20, color='black')
+    plt.ylabel('Frequency Found in Literature', fontsize=20, color='black')
+    plt.title('Distribution of Incubation Periods', fontsize=23, color='black')
+    plt.xlim(1, 62)
+    plt.rcParams['axes.grid'] = True
+    plt.savefig('Charts/IncubHist')
     plt.show()
